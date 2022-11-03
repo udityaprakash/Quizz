101de088f7e467f4addb8a332f5bf866f0de506f
UDITYA PRAKASH <udityaprakash983@gmail.com>
2022-11-03 13:26:01 +0530

    reduced code

--- main.js
+++ main.js
@@ -1,38 +1,11 @@
 var numQues = 15;
 
 var numChoi = 3;
-
-var answers = new Array(15);
-
-answers[0] = "doesn't like";
-
-answers[1] = "don't come";
-
-answers[2] = "come";
-
-answers[3] = "don't";
-
-answers[4] = "doesn't make";
-
-answers[5] = "seem";
-
-answers[6] = "don't have";
-
-answers[7] = "doesn't have";
-
-answers[8] = "takes";
-
-answers[9] = "want";
-
-answers[10] = "comes";
-
-answers[11] = "doesn't know";
-
-answers[12] = "tries";
-
-answers[13] = "play";
-
-answers[14] = "lasts";
+var questions={
+    questions:["He -------------------- it.",],
+    answers:["doesn't like","don't come","come","don't","doesn't make","seem","don't have","doesn't have","takes","want","comes","doesn't know","tries","play","lasts"],
+    options:[[],[],[]]
+}
 
 function getScore(form) {
 
@@ -56,7 +29,7 @@ if (currSelection.checked) {
 
 answered=true;
 
-if (currSelection.value == answers[i]) {
+if (currSelection.value == questions.answers[i]) {
 
 score++;
 
